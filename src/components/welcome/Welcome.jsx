12f4244a0e83653282d8837211e0e088ff464d46
Can Aksakal <aksakalcan97@gmail.com>
2024-05-09 20:39:15 +0300

    fix: imported images

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import "./Welcome.css"
+import React from "react";
+import "./Welcome.css";
+import backround from "/images/corner.jpeg";
 
 const Welcome = () => {
   return (
-    <div id='welcome' className='bg-image'>
-        <h2 className='bg-image-text'>Willkommen im Haus der Ringlers</h2>
+    <div
+      id="welcome"
+      className="bg-image"
+      style={{
+        backgroundImage: `url("${backround}")`,
+      }}
+    >
+      <h2 className="bg-image-text">Willkommen im Haus der Ringlers</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
